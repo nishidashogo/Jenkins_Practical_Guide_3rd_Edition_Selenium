827056fe251220acfd577ec5064b54497d0bdd20
nishidashogo <73509674+nishidashogo@users.noreply.github.com>
2022-08-25 14:27:53 +0900

    Update SampleTestCase.java

--- src/test/java/jp/gihyo/jenkinsbook/webdriver/SampleTestCase.java
+++ src/test/java/jp/gihyo/jenkinsbook/webdriver/SampleTestCase.java
@@ -13,7 +13,6 @@ import jp.gihyo.jenkinsbook.page.TopPage;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeOptions;
@@ -32,11 +31,19 @@ public class SampleTestCase {
 	@BeforeClass
 	public static void setUpClass() throws IOException {
 		prop.load(new FileInputStream("target/test-classes/selenium.properties"));
-		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("headless");
-		options.addArguments("--no-sandbox");
-		driver = new ChromeDriver(options);
+		String myProjectARN = "arn:aws:devicefarm:us-west-2:421850136157:testgrid-project:0298e20b-5d0f-43b2-9517-cad66a36f6a5";
+		DeviceFarmClient client  = DeviceFarmClient.builder().region(Region.US_WEST_2).build();
+		CreateTestGridUrlRequest request = CreateTestGridUrlRequest.builder()
+		.expiresInSeconds(300)
+		.projectArn(myProjectARN)
+		.build();
+		CreateTestGridUrlResponse response = client.createTestGridUrl(request);
+		DesiredCapabilities cap = DesiredCapabilities.edge();
+		cap.setCapability("ms:edgeChromium", "true");
+		URL testGridUrl = new URL(response.url());
+		// You can now pass this URL into RemoteWebDriver.
+		driver = new RemoteWebDriver(testGridUrl, cap);
+
 	}
 	
 	@AfterClass
